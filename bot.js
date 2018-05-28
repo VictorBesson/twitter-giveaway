a95ef9d5165e1842b0ec8219d9a3d34db3dcd501
Rulty <none>
2018-05-28 21:21:49 +0200

    Finich like mention, limit rate

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ var config = require('./config.js');
 var T = new Twit(config);
 
 //Begin a stream for each status with the world giveaway
-var stream = T.stream('statuses/filter', { track: 'giveaway rt suivre, giveaway rt follow', language: 'fr' })
+var stream = T.stream('statuses/filter', { track: 'giveaway rt suivre, giveaway rt follow'})
 
 function retweet(tweet_id){
   T.post('statuses/retweet/:id', { id: tweet_id }, function (err, data, response) {
@@ -13,7 +13,7 @@ function retweet(tweet_id){
 }
 
 function fav(tweet_id){
-  T.post('favorites/create/:id', { id: tweet_id }, function (err, data, response){
+  T.post('favorites/create', { id: tweet_id }, function (err, data, response){
     //In the like
     console.log('Like n°' + tweet_id);
   })
@@ -21,21 +21,41 @@ function fav(tweet_id){
 
 function like(user_id){
   T.post('friendships/create', { id: user_id }, function (err, data, response){
-    console.log(user_id);
+    console.log('User n°' + user_id);
   })
 }
 
 //On each tweet in realtime, begin this function
 stream.on('tweet', function (tweet) {
-  if(tweet.hasOwnProperty('retweeted_status') == false && tweet.is_quote_status == false){
-    //retweet like tweet and like every person on mention
+  if(tweet.is_quote_status){
+    //if it's a quote, get the original tweet
+    tweet = tweet.quoted_status;
+  }
+  if(tweet.hasOwnProperty('retweeted_status')){
+    //or it's a retweet
+    tweet = tweet.retweeted_status;
+  }
+  //retweet like tweet and like every person on mention
+  if(tweet.retweeted == false){
     retweet(tweet.id_str);
+  }
+  if(tweet.favorited == false){
     fav(tweet.id_str);
-    like(tweet.user.id_str)
-    for(let user in tweet.entities.user_mention){
-      like(user.id_str);
-    }
+    like(tweet.user.id_str);
+  }
+//Check all mentions and like
+  tweet.entities.user_mentions.forEach(function(element){
+    like(element.id_str);
+  })
 
-}
+})
 
+stream.on('limit', function (limitMessage) {
+  console.log(limitMessage);
+})
+
+stream.on('error', function (event) {
+  stream.stop;
+  console.log("Limit Rate, 15 min of waiting...");
+  setTimeOut(stream.start, 900000);
 })
