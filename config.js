19203430bcab8488ffd47c288d5534a6a904ba2c
Rulty <none>
2018-05-28 17:37:47 +0200

    adding twit adding twit + first tweet hello

---
+++ config.js
@@ -0,0 +1,14 @@
+//Bruno19077587
+//config.js
+/** TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ */
+module.exports = {
+  consumer_key: 'Lcfetlkb0ObhjUYvymUtf98Yt',
+  consumer_secret: 'ioY1DdYchKeKI06EbYpT7mM8KJ7uqt1arZBgQ7CqSEB24DcvOZ',
+  access_token: '1000361177678057472-acrLdmCu9cWVNQnGfHiHS1Qbo7kc26',
+  access_token_secret: '9sLbf7tv3dbHWcJvv3F38rXebKH1euqYCfQOU4CgbZfjT'
+}
