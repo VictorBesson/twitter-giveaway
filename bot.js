02ac1d38bfe5b31c1169b5627926beced1fc30b3
Rulty <none>
2018-05-29 16:57:07 +0200

    small modif

--- bot.js
+++ bot.js
@@ -1,9 +1,12 @@
 var Twit = require('twit');
+
 var config = require('./config.js');
+
 var T = new Twit(config);
 
 //Begin a stream for each status with the world giveaway
 var stream = T.stream('statuses/filter', { track: 'giveaway rt suivre, giveaway rt follow'})
+var count =0;
 
 function retweet(tweet_id){
   T.post('statuses/retweet/:id', { id: tweet_id }, function (err, data, response) {
@@ -15,6 +18,10 @@ function retweet(tweet_id){
 function fav(tweet_id){
   T.post('favorites/create', { id: tweet_id }, function (err, data, response){
     //In the like
+    T.post('statuses/retweet/:id', { id: tweet_id }, function (err, data, response) {
+      //Something to do after retweet
+      console.log('Retweet n°' + tweet_id);
+    })
     console.log('Like n°' + tweet_id);
   })
 }
@@ -37,16 +44,18 @@ stream.on('tweet', function (tweet) {
   }
   //retweet like tweet and like every person on mention
   if(tweet.retweeted == false){
-    retweet(tweet.id_str);
-  }
-  if(tweet.favorited == false){
     fav(tweet.id_str);
+    //retweet(tweet.id_str);
+
     like(tweet.user.id_str);
+    count++;
   }
+
 //Check all mentions and like
   tweet.entities.user_mentions.forEach(function(element){
     like(element.id_str);
   })
+
 
 })
 
@@ -57,5 +66,6 @@ stream.on('limit', function (limitMessage) {
 stream.on('error', function (event) {
   stream.stop;
   console.log("Limit Rate, 15 min of waiting...");
+  console.log("Number of retweet : " + count);
   setTimeOut(stream.start, 900000);
 })
--- config.js
+++ config.js
@@ -9,6 +9,6 @@
 module.exports = {
   consumer_key: 'Lcfetlkb0ObhjUYvymUtf98Yt',
   consumer_secret: 'ioY1DdYchKeKI06EbYpT7mM8KJ7uqt1arZBgQ7CqSEB24DcvOZ',
-  access_token: '1000361177678057472-acrLdmCu9cWVNQnGfHiHS1Qbo7kc26',
-  access_token_secret: '9sLbf7tv3dbHWcJvv3F38rXebKH1euqYCfQOU4CgbZfjT'
+  access_token: '1000361177678057472-M4kIsYoAbeqwIPGG4RFHCZpYrSFa50',
+  access_token_secret: 'jXQQPMH6grsTMbOs3Y3GUoxCW8VLy3H9T4AfBFrMhdP5k'
 }
